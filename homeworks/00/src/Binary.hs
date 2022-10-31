dd7cfd29fee5a247c4d94f59519bcff5e5d2f189
NaBivatoR <mmsva21@gmail.com>
2022-10-31 19:54:31 +0200

    modified:   homeworks/00/src/Binary.hs

--- homeworks/00/src/Binary.hs
+++ homeworks/00/src/Binary.hs
@@ -11,22 +11,44 @@ data Bit = Zero | One
 infixl 6 :.
 
 succBinary :: Binary -> Binary
-succBinary = undefined
+succBinary End = End :. One 
+succBinary (a :. Zero) = a :. One
+succBinary (a :. One) = succBinary a :. Zero
 
 integerToBinary :: Integer -> Binary
-integerToBinary = undefined
+integerToBinary 0 = End:.Zero
+integerToBinary 1 = End:.One
+integerToBinary a = 
+    if rem a 2 == 1
+        then integerToBinary (quot a 2) :. One
+        else integerToBinary (quot a 2) :. Zero 
 
 binaryToInteger :: Binary -> Integer
-binaryToInteger = undefined
+binaryToInteger End = 0
+binaryToInteger (a :. Zero) = 2*binaryToInteger a +0
+binaryToInteger (a :. One) = 2*binaryToInteger a + 1
 
 hasLeadingZero :: Binary -> Bool
-hasLeadingZero = undefined
+hasLeadingZero (End :. One)= False
+hasLeadingZero (End :. Zero)=True
+hasLeadingZero (a :. _) = hasLeadingZero a
 
 isEnd :: Binary -> Bool
-isEnd = undefined
+isEnd End = True
+isEnd (End :. _) = False
 
 canonicalise :: Binary -> Binary
-canonicalise = undefined
+canonicalise End = End
+canonicalise a = 
+    if hasLeadingZero a
+        then integerToBinary (binaryToInteger a)
+        else a
 
 addBinary :: Binary -> Binary -> Binary
-addBinary = undefined
+addBinary End End = End
+addBinary End a = a
+addBinary a End = a
+addBinary (a:.Zero) (b:.Zero) = addBinary a b :. Zero
+addBinary (a:.One) (b:.Zero) = addBinary a b :. One
+addBinary (a:.Zero) (b:.One) = addBinary a b :. One
+addBinary (a:.One) (b:.One) = addBinary (a:.One) b:.Zero
